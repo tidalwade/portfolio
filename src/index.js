04b029c75183ce1f6ff9605cc32a2d71bec3074a
tidalwade <ryantwade@hotmail.com>
2021-03-14 22:36:46 -0400

    commented out popper in index js file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import '../node_modules/bootstrap/dist/js/popper.min.js';
+// import '../node_modules/bootstrap/dist/js/popper.min.js';
 
 ReactDOM.render(
   <BrowserRouter>
