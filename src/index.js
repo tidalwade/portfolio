5a3b58cf202efb3537ea3a25e9a1fa3b8e90e569
tidalwade <ryantwade@hotmail.com>
2021-04-13 21:43:31 -0400

    adding fonts and colors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 // import '../node_modules/bootstrap/dist/js/popper.min.js';
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
