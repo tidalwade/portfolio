6b769c8cf46e5d2ad22babc7adc4f872d651e84a
tidalwade <35672042+tidalwade@users.noreply.github.com>
2022-02-13 21:15:38 -0500

    portfolio edits

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import '../node_modules/jquery/dist/jquery.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 // import '../node_modules/bootstrap/dist/js/popper.min.js';
 import "../node_modules/font-awesome/css/font-awesome.min.css";
-
+A
 
 ReactDOM.render(
   <BrowserRouter>
